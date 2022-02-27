8caa1439c068f64fc5fd997bc4f45d6aaeac0bbb
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-02-27 18:33:38 -0500

    Upload New File

---
+++ SpartyGnomeApp.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file ShoimyaChowdhuryApp.cpp
+ * @author Shoimya Chowdhury
+ */
+#include "pch.h"
+#include "MainFrame.h"
+#include "SpartyGnomeApp.h"
+
+/**
+ * Initialize the application.
+ * @return
+ */
+bool SpartyGnomeApp::OnInit()
+{
+    if (!wxApp::OnInit())
+        return false;
+
+    auto frame = new MainFrame();
+    frame->Initialize();
+    frame->Show(true);
+
+
+    return true;
+}
+
