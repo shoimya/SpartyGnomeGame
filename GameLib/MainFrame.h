cdecaed82f3acf4c49c1a41d2e567ca4b15bbb2e
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-02-27 18:30:23 -0500

    Upload New File

---
+++ GameLib/MainFrame.h
@@ -0,0 +1,22 @@
+/**
+ * @file MainFrame.h
+ * @author User
+ *
+ *
+ */
+
+#ifndef SPARTYGNOME_MAINFRAME_H
+#define SPARTYGNOME_MAINFRAME_H
+
+#include <wx/wx.h>
+/**
+ * The top-level (main) frame of the application
+ */
+class MainFrame : public wxFrame {
+private:
+
+public:
+    void Initialize();
+};
+
+#endif //SPARTYGNOME_MAINFRAME_H
