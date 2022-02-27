e79608c6f39e515d9bbe974271936b7d6e219503
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-02-27 18:30:41 -0500

    Upload New File

---
+++ GameLib/pch.h
@@ -0,0 +1,15 @@
+
+/**
+ * @file pch.h
+ * @author User
+ *
+ *
+ */
+
+#ifndef SPARTYGNOME_PCH_H
+#define SPARTYGNOME_PCH_H
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#endif //SPARTYGNOME_PCH_H
