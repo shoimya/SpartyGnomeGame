26ebaee2405ba5ac724a9cc7567bf4f4e8e2539e
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-02-27 18:31:28 -0500

    Upload New File

---
+++ GameLib/StadiumView.h
@@ -0,0 +1,21 @@
+/**
+ * @file StadiumView.h
+ * @author User
+ *
+ *
+ */
+
+#ifndef SPARTYGNOME_STADIUMVIEW_H
+#define SPARTYGNOME_STADIUMVIEW_H
+
+/**
+ * View class for our Game
+ */
+class StadiumView: public wxWindow  {
+private:
+
+public:
+    void Initialize(wxFrame* parent);
+};
+
+#endif //SPARTYGNOME_STADIUMVIEW_H
