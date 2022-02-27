fd62624e7d9b6204565e87c938795f6cb2db9e25
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-02-27 18:33:14 -0500

    Upload New File

---
+++ pch.h
@@ -0,0 +1,14 @@
+/**
+ * @file pch.h
+ * @author User
+ *
+ *
+ */
+
+#ifndef PROJECT1_PCH_H
+#define PROJECT1_PCH_H
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#endif //PROJECT1_PCH_H
