1524943656b014a3f1c142a45eb9d8a2d5c5f12f
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-03-15 18:15:48 -0400

    Update Wall.cpp the functions

--- GameLib/Wall.cpp
+++ GameLib/Wall.cpp
@@ -15,3 +15,26 @@ Wall::Wall(Stadium *Stadium, std::shared_ptr<Picture> Picture) : ItemPlatform(St
 Wall::~Wall(){
 
 }
+
+void Wall::Draw(std::shared_ptr<wxGraphicsContext> graphics)
+{
+    ItemPlatform::Draw(graphics);
+}
+
+void Wall::Draw(std::shared_ptr<wxGraphicsContext> graphics, double XOffSet, double yOffSet)
+{
+    ItemPlatform::Draw(graphics, XOffSet, yOffSet);
+}
+
+void Wall::XmlLoad(wxXmlNode* node, int xPos)
+{
+    long x, y = 0;
+    double hit, wid = 0.0;
+    node->GetAttribute(L"x", L"0").ToLong(&x);
+    node->GetAttribute(L"y", L"0").ToLong(&y);
+    node->GetAttribute(L"width").ToDouble(&wid);
+    node->GetAttribute(L"height").ToDouble(&hit);
+    SetWidth(wid);
+    SetHeight(hit);
+    SetLocation(x,y);
+}
