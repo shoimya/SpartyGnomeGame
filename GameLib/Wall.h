851c1d387126061d73902bffa6f13db233187f84
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-03-16 13:42:10 -0400

    Update Wall.h after implementing the load

--- GameLib/Wall.h
+++ GameLib/Wall.h
@@ -14,23 +14,13 @@
 
 class Wall : public ItemPlatform{
 private:
-    Picture *mPicture;
-    
-    double mWidth = 0.0;
-    double mHeight = 0.0;
 
 public:
     Wall(Stadium *Stadium, std::shared_ptr<Picture> picture);
-    void Accept(Visitor *visitor) override{};
     ~Wall();
     
     void Draw(std::shared_ptr<wxGraphicsContext> graphics) override;
-    void Draw(std::shared_ptr<wxGraphicsContext> graphics, double XOffSet, double yOffSet);
-    
-    void XmlLoad(wxXmlNode *node, int xPos);
-    void SetHeight(double hei) {mHeight = hei;}
-    void SetWidth(double wid) {mWidth = wid;}
-
+    void XmlLoad(wxXmlNode *node) override;
 
 };
 
