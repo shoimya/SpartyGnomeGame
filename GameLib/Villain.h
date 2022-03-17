1c6eaac3a6a25ac1bdb304992b9a9bf49b818221
Connortif1463 <ctfischetti@gmail.com>
2022-03-17 13:18:34 -0400

    Villain Movement and Loading

---
+++ GameLib/Villain.h
@@ -0,0 +1,40 @@
+/**
+ * @file Villain.h
+ * @author Connor
+ *
+ *
+ */
+
+#ifndef SPARTYGNOME_VILLAIN_H
+#define SPARTYGNOME_VILLAIN_H
+
+#include "Item.h"
+#include "Visitor.h"
+
+class Villain : public Item{
+private:
+    Picture *picture;
+
+    /// Fish speed in the Y direction
+    /// in pixels per second
+    double mSpeedY = -240;
+
+    /// Initial Item location in stadium
+    Vector mPosInit;
+
+public:
+    Villain(Stadium* stadium, std::shared_ptr<Picture> picture);
+    void Draw(std::shared_ptr<wxGraphicsContext> graphics) override;
+    void Accept(Visitor* visitor) override {}
+    ~Villain();
+
+    void XmlLoad(wxXmlNode *node) override;
+
+    void Update(double elapsed);
+
+    void SetInitialLocation(double x, double y) { mPosInit.Set(x,y); }
+
+//    void SetLocation(double x, double y) { mPos.Set(x,y); }
+};
+
+#endif //SPARTYGNOME_VILLAIN_H
