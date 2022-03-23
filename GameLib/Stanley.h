c73d1d7a930de5cc6ca312f819e15f5087ce4267
Sarah Swann <swannsar@msu.edu>
2022-03-22 23:20:53 -0400

    doxygen fixes

--- GameLib/Stanley.h
+++ GameLib/Stanley.h
@@ -11,26 +11,57 @@
 #include "Item.h"
 #include "Visitor.h"
 
+/**
+ * Class for Stanley item
+*/
 class Stanley : public Item{
 private:
-    bool mHit = false; /// Money hit indicator
+    /// Money hit indicator
+    bool mHit = false;
 
-    double mDuration = 0; /// Frame duration in milliseconds
+    /// Frame duration in milliseconds
+    double mDuration = 0;
 
-    double mMoneySpeed = 300; /// Money speed in pixels per second
+    /// Money speed in pixels per second
+    double mMoneySpeed = 300;
 
-    wxTimer mFlyTime; /// Timer for floating money
+    /// Timer for floating money
+    wxTimer mFlyTime;
 
-    wxStopWatch mStopWatch; /// Stopwatch used to measure elapsed time
+    /// Stopwatch used to measure elapsed time
+    wxStopWatch mStopWatch;
 public:
+    /**
+     * Stanley constructor
+     * @param stadium the stadium Stanley is a part of
+     * @param picture the picture for Stanley
+     */
     Stanley(Stadium* stadium, std::shared_ptr<Picture> picture);
+    /**
+     * Draw Stanley on a graphics context
+     * @param graphics Graphics context to draw on
+     */
     void Draw(std::shared_ptr<wxGraphicsContext> graphics) override;
+
+    /**
+     * Accept a visitor
+     * @param visitor we accept
+     */
     void Accept(Visitor* visitor) override {visitor->VisitStanley(this);}
 
+    /**
+     * Checks if money is hit
+     * @param item item we hit
+     * @return bool value if hit is true/false
+     */
     bool CollisionTest(Item* item) override;
 
     ~Stanley();
 
+    /**
+     * Checks if money is hit
+     * @param elapsed time since last update
+     */
     void Update(double elapsed) override;
 };
 
