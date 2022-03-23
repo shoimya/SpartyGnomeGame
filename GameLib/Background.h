ff2dcd7374814491b2ec9b9b8290852995887131
Shoimya Chowdhury <chowdh83@cse.msu.edu>
2022-03-22 21:29:59 -0400

    Update Background.h

--- GameLib/Background.h
+++ GameLib/Background.h
@@ -10,14 +10,18 @@
 
 #include "Item.h"
 #include "Visitor.h"
-
+/*
+* Background class to load object 
+**/
 class Background : public Item{
 private:
     Picture* mPicture;
 public:
+    /// constructor
     Background(Stadium* stadium, std::shared_ptr<Picture> picture);
+    ///destructor
     ~Background();
-
+    
     void Draw(std::shared_ptr<wxGraphicsContext> graphics) override;
     void Accept(Visitor* visitor) override {visitor->VisitBackground(this);}
 
